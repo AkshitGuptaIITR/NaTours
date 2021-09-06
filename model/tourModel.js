ba77040f04c399e8f797ad73da020e0bc9556feb
Akshit <Gakshit92@gmail.com>
2021-09-06 21:46:46 +0530

    New Fields

--- model/tourModel.js
+++ model/tourModel.js
@@ -6,28 +6,59 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Name is required'],
-    unique: true
+    unique: true,
+    trim: true
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Must Have A Duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'It should have difficulty']
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'Price is required']
   },
+  priceDiscount: {
+    type: Number
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Tour must have a cover image'],
+  },
+  image: {
+    type: [String]
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
-
-// const testTour = new Tour({
-//   name: 'The Forest Hiker',
-//   rating: 4.7,
-//   // price: 488
-// });
-
-// testTour.save().then((doc) =>{
-//   console.log(doc)
-// }).catch((err) => console.log(err));
