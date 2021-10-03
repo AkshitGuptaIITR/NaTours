0f5981cb5bdb9c46c2760ea061adf68444edb06a
Akshit <Gakshit92@gmail.com>
2021-10-04 00:32:21 +0530

    referncing the guides and populatting them

--- model/tourModel.js
+++ model/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 
 //Creating the mongoDB Schema Table for your database
@@ -110,6 +111,12 @@ const tourSchema = new mongoose.Schema({
       description: String,
       day: Number
     }
+  ],
+  guides: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: User
+    }
   ]
 }, {
   toJSON: { virtuals: true },
@@ -129,6 +136,17 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
+//? This is the embedding of the users in tours 
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id))
+
+  //* Promise.all get the input of all the promises of an array at the same time
+  this.guides = await Promise.all(guidesPromises);
+
+  next();
+})
+
 // * Runs before save command
 // tourSchema.post('save', function (doc, next) {
 //   console.log(doc);
@@ -140,6 +158,15 @@ tourSchema.pre('save', function (next) {
 
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
+  next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+  //? populate() helps us to fill up the data of the referenced fields like guides in this case
+  this.populate({
+    path: 'guides',
+    // select: '-__v -passwordChangedAt'
+  });
   next();
 })
 
